1f62bad02d318b88590ac9988efef5936a8f2d5b
islamhasib953 <islamhasib953@example.com>
2024-08-07 19:09:28 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { createUser, uploadPhoto } from "./utils";
 
-export default function handleProfileSignup(){
+export default function handleProfileSignup() {
 	return Promise
-	.all([uploadPhoto(), createUser()])
-	.then((values) =>{
-			console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-		})
-	.catch(() => {console.log('Signup system offline');
+		.all([uploadPhoto(), createUser()])
+		.then((values) =>{
+				console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+			})
+		.catch(() => {console.log('Signup system offline');
 	});
 }
