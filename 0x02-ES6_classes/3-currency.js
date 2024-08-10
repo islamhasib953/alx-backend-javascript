9899a3a47f374e5a2f6326cfb8b6349bf9232ee6
islamhasib953 <islamhasib953@example.com>
2024-08-10 16:07:50 +0300

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set code(Newcode) {
+    this._code = Newcode;
+  }
+  set name(Newname) {
+    this._name = Newname;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
