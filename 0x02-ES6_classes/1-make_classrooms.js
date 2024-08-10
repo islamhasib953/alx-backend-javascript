094d9006c39c15ecbfe55ea061ecdf1ae858d09b
islamhasib953 <islamhasib953@example.com>
2024-08-10 15:21:43 +0300

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,5 +4,5 @@ export default function initializeRooms() {
   const Room1 = new ClassRoom(19);
   const Room2 = new ClassRoom(20);
   const Room3 = new ClassRoom(34);
-  return [Room1, Room2, Room3]
+  return [Room1, Room2, Room3];
 }
